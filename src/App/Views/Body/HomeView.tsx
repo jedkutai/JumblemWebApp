48392cccafdc8cfcf711fb1fa9814e0f0ba79ae4
Jed Kutai <jedkutai@gmail.com>
2024-12-19 22:14:15 -0500

    testing site3

--- src/App/Views/Body/HomeView.tsx
+++ src/App/Views/Body/HomeView.tsx
@@ -111,6 +111,7 @@ export default function HomeView({
         <View>
             <VStack>
                 <JumblemLogoSimple />
+                <Typography>Test</Typography>
                 <Box style={styles.section}>
                     <Typography style={styles.sectionTitle}>VERSUS</Typography>
                     <Box style={styles.buttonContainer}>
