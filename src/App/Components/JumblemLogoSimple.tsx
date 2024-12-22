c3b1f0570f7b640af11ce322913b55d53dbfc557
Jed Kutai <jedkutai@gmail.com>
2024-12-21 19:13:35 -0500

    jts logo but smaller and as png

--- src/App/Components/JumblemLogoSimple.tsx
+++ src/App/Components/JumblemLogoSimple.tsx
@@ -1,19 +1,19 @@
 
-import jumblemLogoSimpleImage from "../../assets/drawingexport.svg";
-// import jumblemLogoSimpleImage from "../../assets/drawing.svg";
+import jumblemLogoSimpleImage from "../../assets/drawingexportresize.png";
+import { useWindowSize } from "../../Background/Utils/useWindowSize";
 
 export default function JumblemLogoSimple() {
-    // const { minDimension } = useWindowSize();
-    // const style = {
-    //     maxWidth: `${Math.min(minDimension / 3, 300)}px`,
-    //     maxHeight: `${Math.min(minDimension / 3, 200)}px`,
-    //     marginBottom: "20px",
-    // };
+    const { minDimension } = useWindowSize();
     const style = {
-        maxWidth: "300px",
-        maxHeight: "200px",
-        margin: "20px",
+        maxWidth: `${Math.min(minDimension / 3, 300)}px`,
+        maxHeight: `${Math.min(minDimension / 3, 200)}px`,
+        marginBottom: `${Math.min(minDimension / 3, 200)/10}px`,
     };
+    // const style = {
+    //     maxWidth: "300px",
+    //     maxHeight: "200px",
+    //     margin: "20px",
+    // };
 
     return (
         <img src={jumblemLogoSimpleImage} alt="Jumblem Logo" style={style} />
