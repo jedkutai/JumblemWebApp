99a52dda35582759e575d7f859f4b5e82796f983
Jed Kutai <jedkutai@gmail.com>
2025-01-01 09:03:48 -0500

    how did you get here? no comments

--- src/App/Components/Embeds/HowDidIGetHere.tsx
+++ src/App/Components/Embeds/HowDidIGetHere.tsx
@@ -8,7 +8,7 @@ export default function HowDidIGetHere() {
                 scrolling="no"
                 frameBorder="no"
                 allow="autoplay"
-                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/55149736&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/55149736&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=true">
             </iframe>
             <div
                 style={{
