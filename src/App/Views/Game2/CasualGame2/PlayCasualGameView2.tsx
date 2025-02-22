8e2b8e671b3a8b74e8ac3e8583feb6d15c529f22
jedkutai <jedkutai@gmail.com>
2025-02-22 17:05:15 -0500

    quick update6

--- src/App/Views/Game2/CasualGame2/PlayCasualGameView2.tsx
+++ src/App/Views/Game2/CasualGame2/PlayCasualGameView2.tsx
@@ -236,7 +236,7 @@ export default function PlayCasualGameView2({ passedUser, passedGame }: PlayCasu
                 <VStack width={`${width}px`} height={`${height}px`}>
                     <HowToPlayHeader versus={true} />
                     <JumblemLogoSimple />
-                    <p>caca</p>
+                    <p>michaels pc is cursed</p>
                     <CasualGameHeader2
                         setUserTimeExpired={setUserTimeExpired}
                         setCheckOpponentTimeExpired={setCheckOpponentTimeExpired}
