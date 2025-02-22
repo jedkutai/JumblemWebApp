1dee8d6e4eab5174de730f83c3b6237984b64bb9
jedkutai <jedkutai@gmail.com>
2025-02-22 16:37:32 -0500

    quick update2

--- src/App/Views/Game/PrivateMatch/PrivateGameBlock.tsx
+++ src/App/Views/Game/PrivateMatch/PrivateGameBlock.tsx
@@ -1,54 +1,54 @@
-// import { GridSpotModel, MoveModel } from "../../../../Background/Models";
-// import { YellowLetterBlock, WhiteLetterBlock, EmptyBlock, EmptySelectedBlock, EmptyValidBlock } from "../../../Components";
+import { GridSpotModel, MoveModel } from "../../../../Background/Models";
+import { YellowLetterBlock, WhiteLetterBlock, EmptyBlock, EmptySelectedBlock, EmptyValidBlock } from "../../../Components";
 
 
-// interface PrivateGameBlockProps {
-//     block: GridSpotModel
-//     availableBlocks: string[];
-//     wordCheckComplete: boolean;
-//     moves: Record<string, MoveModel>;
-//     selectedBlock: string;
-//     setSelectedBlock: (selectedBlock: string) => void;
-//     yourTurn: boolean;
-//     blockDimension: number;
-//     lastMove: MoveModel | undefined;
+interface PrivateGameBlockProps {
+    block: GridSpotModel
+    availableBlocks: string[];
+    wordCheckComplete: boolean;
+    moves: Record<string, MoveModel>;
+    selectedBlock: string;
+    setSelectedBlock: (selectedBlock: string) => void;
+    yourTurn: boolean;
+    blockDimension: number;
+    lastMove: MoveModel | undefined;
 
-// }
+}
 
-// export default function PrivateGameBlock({
-//     block,
-//     availableBlocks,
-//     wordCheckComplete,
-//     moves,
-//     selectedBlock,
-//     setSelectedBlock,
-//     yourTurn,
-//     blockDimension,
-//     lastMove
-// }: PrivateGameBlockProps) {
+export default function PrivateGameBlock({
+    block,
+    availableBlocks,
+    wordCheckComplete,
+    moves,
+    selectedBlock,
+    setSelectedBlock,
+    yourTurn,
+    blockDimension,
+    lastMove
+}: PrivateGameBlockProps) {
     
 
 
-//     return (
-//         <>
-//             {(availableBlocks.includes(block.id) && Object.keys(moves).includes(block.id) && lastMove && lastMove.coordinates === block.id) && (
-//                 <YellowLetterBlock letter={moves[block.id].letter} blockDimension={blockDimension} />
-//             )}
-//             {(availableBlocks.includes(block.id) && Object.keys(moves).includes(block.id) && lastMove && lastMove.coordinates !== block.id) && (
-//                 <WhiteLetterBlock letter={moves[block.id].letter} blockDimension={blockDimension} />
-//             )}
-//             {(availableBlocks.includes(block.id) && !Object.keys(moves).includes(block.id) && (!yourTurn || !wordCheckComplete)) && (
-//                 <EmptyBlock blockDimension={blockDimension} />
-//             )}
-//             {(availableBlocks.includes(block.id) && !Object.keys(moves).includes(block.id) && yourTurn && wordCheckComplete && (selectedBlock === block.id)) && (
-//                 <EmptySelectedBlock blockDimension={blockDimension} setSelectedBlock={setSelectedBlock}/>
-//             )}
-//             {(availableBlocks.includes(block.id) && !Object.keys(moves).includes(block.id) && yourTurn && wordCheckComplete && (selectedBlock !== block.id)) && (
-//                 <EmptyValidBlock blockDimension={blockDimension} setSelectedBlock={setSelectedBlock} blockId={block.id}/>
-//             )}
-//             {(!availableBlocks.includes(block.id)) && (
-//                 <EmptyBlock blockDimension={blockDimension} />
-//             )}
-//         </>
-//     )
-// }
+    return (
+        <>
+            {(availableBlocks.includes(block.id) && Object.keys(moves).includes(block.id) && lastMove && lastMove.coordinates === block.id) && (
+                <YellowLetterBlock letter={moves[block.id].letter} blockDimension={blockDimension} />
+            )}
+            {(availableBlocks.includes(block.id) && Object.keys(moves).includes(block.id) && lastMove && lastMove.coordinates !== block.id) && (
+                <WhiteLetterBlock letter={moves[block.id].letter} blockDimension={blockDimension} />
+            )}
+            {(availableBlocks.includes(block.id) && !Object.keys(moves).includes(block.id) && (!yourTurn || !wordCheckComplete)) && (
+                <EmptyBlock blockDimension={blockDimension} />
+            )}
+            {(availableBlocks.includes(block.id) && !Object.keys(moves).includes(block.id) && yourTurn && wordCheckComplete && (selectedBlock === block.id)) && (
+                <EmptySelectedBlock blockDimension={blockDimension} setSelectedBlock={setSelectedBlock}/>
+            )}
+            {(availableBlocks.includes(block.id) && !Object.keys(moves).includes(block.id) && yourTurn && wordCheckComplete && (selectedBlock !== block.id)) && (
+                <EmptyValidBlock blockDimension={blockDimension} setSelectedBlock={setSelectedBlock} blockId={block.id}/>
+            )}
+            {(!availableBlocks.includes(block.id)) && (
+                <EmptyBlock blockDimension={blockDimension} />
+            )}
+        </>
+    )
+}
